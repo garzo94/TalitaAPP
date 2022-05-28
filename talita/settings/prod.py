552e672d9534300d953394f6fe03c5fd28b83550
garzo94 <alexgarzo25@gmail.com>
2022-05-27 21:17:01 -0600

    bucket name variable

--- talita/settings/prod.py
+++ talita/settings/prod.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = ['talita-app.herokuapp.com']
 # Amazon sttings
 AWS_ACCES_KEY_ID = config('AWS_SECRET_ACCES_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = f'(AWS_STORAGE_BUCKET_NAME).s3_amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3_amazonaws.com'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl':'max-age=86400'
