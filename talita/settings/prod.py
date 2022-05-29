ced472643d33085843b30283e2ca25a6f969cec6
garzo94 <alexgarzo25@gmail.com>
2022-05-29 13:35:00 -0600

    sdf

--- talita/settings/prod.py
+++ talita/settings/prod.py
@@ -5,13 +5,13 @@ import django_on_heroku
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['talita-app.herokuapp.com']
 
 # Amazon sttings
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_ACCES_KEY_ID = config('AWS_ACCES_KEY_ID')
+AWS_ACCES_KEY_ID = config('AWS_SECRET_ACCES_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3_amazonaws.com'
 AWS_DEFAULT_ACL = 'public-read'
@@ -45,7 +45,7 @@ LOGGING = {
     'handlers': {
         'console': {
             'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
+            'class': 'logging.FileHandler',
       
         },
     },
