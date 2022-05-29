e22341ec30a8f273fea2261e2a6db9e852bb675b
garzo94 <alexgarzo25@gmail.com>
2022-05-29 14:35:14 -0600

    sfsdf

--- talita/settings/prod.py
+++ talita/settings/prod.py
@@ -24,6 +24,8 @@ AWS_HEADERS = {
     'Access-Control-Allow-Origin':'*',
 }
 
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
